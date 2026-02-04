f0a3ce10062cb3784681947d06b074d387dfae4c
dhanvant-ip41 <codedhanvant@gmail.com>
2026-02-04 09:22:06 +0530

    Dhanvant

--- PMS/src/main/java/com/example/PMS/DTO/ApplicationDTO/ApplicationStatusResponseDTO.java
+++ PMS/src/main/java/com/example/PMS/DTO/ApplicationDTO/ApplicationStatusResponseDTO.java
@@ -1,5 +1,12 @@
 package com.example.PMS.DTO.ApplicationDTO;
 
-public class ApplicationStatusResponseDTO {
+import java.time.LocalDateTime;
+import lombok.Data;
 
+@Data
+public class ApplicationStatusResponseDTO {
+    private String status;
+    private LocalDateTime updatedAt;
+    private String remarks;
 }
+
