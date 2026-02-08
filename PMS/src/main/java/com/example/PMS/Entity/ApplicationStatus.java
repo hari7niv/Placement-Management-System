7889d598c4a42347706990c7ecb7f8fa1a77b4be
dhanvant-ip41 <codedhanvant@gmail.com>
2026-02-08 20:20:52 +0530

    ApplicationStatusClass-Updated

--- PMS/src/main/java/com/example/PMS/Entity/ApplicationStatus.java
+++ PMS/src/main/java/com/example/PMS/Entity/ApplicationStatus.java
@@ -4,7 +4,12 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.example.PMS.DTO.ApplicationStatusDTO;
+import com.example.PMS.Enum.Status;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,10 +29,22 @@ public class ApplicationStatus {
     @JoinColumn(name = "application_id")
     private Applications application;
 
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
     @UpdateTimestamp
     private LocalDateTime updated_at;
 
     private String remarks;
+
+    public ApplicationStatusDTO toDTO() {
+        ApplicationStatusDTO asd = new ApplicationStatusDTO();
+        asd.setAid(status_id);
+        asd.setRemarks(remarks);
+        asd.setStatus(status);
+        return asd;
+    }
 }
+
+// create a dto with remarks applicationid and status and then return itto the
+// get function after comming form restroom
--- PMS/src/main/java/com/example/PMS/Service/ApplicationStatusService.java
+++ PMS/src/main/java/com/example/PMS/Service/ApplicationStatusService.java
@@ -1,8 +1,13 @@
 package com.example.PMS.Service;
 
+import java.util.Arrays;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.PMS.DTO.ApplicationStatusDTO;
+import com.example.PMS.Entity.ApplicationStatus;
+import com.example.PMS.Enum.Status;
 import com.example.PMS.Repository.ApplicationStatusRepository;
 
 @Service
@@ -13,6 +18,51 @@ public class ApplicationStatusService {
         this.repo = repo;
     }
 
-    
-    
+    public String addStatus(ApplicationStatus apps) {
+        repo.save(apps);
+        return "Added To DB";
+    }
+
+    public ApplicationStatusDTO getStatus(Long id) {
+        return repo.findById(id).stream()
+                .map(a -> a.toDTO())
+                .findFirst()
+                .orElse(null);
+
+    }
+
+    public String updateStatus(Long id, ApplicationStatusDTO app) {
+        ApplicationStatus as = repo.findById(id).orElse(null);
+        if (as != null) {
+            as.setStatus(app.getStatus());
+            as.setRemarks(app.getRemarks());
+            repo.save(as);
+            return "UPDATED";
+        }
+        return "ID NOT FOUND";
+    }
+
+    public String updateStatusOnly(Long id, String status) {
+        ApplicationStatus as = repo.findById(id).orElse(null);
+        if (as == null) {
+            return "ID NOT FOUND";
+        }
+        // USING TRY CATCH
+        // try{
+        // as.setStatus(Status.valueOf(status));
+        // return "UPDATED SUCCESSFULLY";
+        // }catch(Exception e){
+        // return "NOT UPDATED\nERROR: INVALID STATUS VALUE";
+        // }
+
+        // USING ARRAY.STREAM
+        Optional<Status> stat = Arrays.stream(Status.values()).filter(a -> a.name().equalsIgnoreCase(status))
+                .findFirst();
+        if (stat.isPresent()) {
+            as.setStatus(Status.valueOf(status.toUpperCase()));
+            repo.save(as);
+            return "UPDATED SUCCESSFULLY";
+        }
+        return "NOT UPDATED\nERROR: INVALID STATUS VALUE";
+    }
 }
